914f839478495cda565d0ee2416ae6f262fd2556
aeki <aekiwow@hotmail.fr>
2020-03-19 20:38:55 +0100

    register

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,4 +12,3 @@ import $ from 'jquery'
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 // import $ from 'jquery';
 
-console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
